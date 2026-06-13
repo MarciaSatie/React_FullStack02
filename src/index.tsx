440285b2a8827ccc2b4271b7b63d5e9e799d6a87
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2026-06-13 23:47:00 +0100

    Change site header.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
+
 
 
 const App = () => {
   return (
     <BrowserRouter>
+      <SiteHeader />
       <Routes>
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
